18639e476e09137ed9e27dee08c208399c96188c
Krishna Kumar Prajapati <kkp785216@gmail.com>
2024-03-12 21:06:27 +0530

    3tut

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { Fragment, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react";
 
-function App() {
+function App({ children }) {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <main className="flex text-white">
-        <div>This is Tutorial 2</div>
-      </main>
+      <main className="flex ">{children}</main>
     </div>
   );
 }
